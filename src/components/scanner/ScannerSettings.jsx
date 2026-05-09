d0cd6f9ddeac6185761bfb65954101f396db4de3
ami3go_666 <ami3go_666@yahoo.com>
2026-05-09 08:01:08 +0000

    File changes

--- src/components/scanner/ScannerSettings.jsx
+++ src/components/scanner/ScannerSettings.jsx
@@ -38,6 +38,8 @@ export default function ScannerSettings({ settings, onChange, onClose }) {
                 <SelectValue />
               </SelectTrigger>
               <SelectContent>
+                <SelectItem value="1m">1 minut</SelectItem>
+                <SelectItem value="3m">3 minute</SelectItem>
                 <SelectItem value="5m">5 minute</SelectItem>
                 <SelectItem value="15m">15 minute</SelectItem>
                 <SelectItem value="30m">30 minute</SelectItem>
