ce9dcf1693076ed9dffc791dbbfdaabeb9391306
ami3go_666 <ami3go_666@yahoo.com>
2026-05-09 10:39:17 +0000

    File changes

--- base44/functions/testApiKey/entry.ts
+++ base44/functions/testApiKey/entry.ts
@@ -24,8 +24,8 @@ Deno.serve(async (req) => {
     }
     
     const decrypted = await base44.asServiceRole.functions.invoke('decryptApiSecret', { keyId });
-    const apiSecret = decrypted.data?.secret || decrypted.api_secret;
-    const apiPassphrase = decrypted.data?.passphrase || decrypted.api_passphrase;
+    const apiSecret = decrypted.data?.secret || decrypted.secret;
+    const apiPassphrase = decrypted.data?.passphrase || decrypted.passphrase;
     
     if (!apiPassphrase) {
       throw new Error('Trading Passphrase is missing - update your API key with the 6-digit passphrase');
